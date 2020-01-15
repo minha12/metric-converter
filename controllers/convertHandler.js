9b6d5dc32e73aa353f39aa044e8896dcd16d8aa1
Glitch (mmetric) <none>
2020-01-15 19:58:07 +0000

    🐬☺️ Checkpoint ./controllers/convertHandler.js:9419671/4293

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,42 +7,49 @@
  */
 
 function ConvertHandler() {
-  const splitReg = /[\d./]+|[a-zA-Z]+/;
-  const units = ['gal', 'L', 'lbs', 'kg', 'mi', 'km']
-  
+  const splitReg = /[\d\.\/]+|[a-zA-Z]+/;
+  const units = ["gal", "L", "lbs", "kg", "mi", "km"];
+
   this.getNum = function(input) {
     const splitedInput = input.match(splitReg);
-    if(units.includes(input)) {
-      return 1
-    } else{
-      
-      if(splitedInput.length === 2) {
-        const number = splitedInput[0]
-        const isValidNumberReg = /^\d*\.?\d*\/?\d*\.?\d*$/
+    if (units.includes(input)) {
+      return 1;
+    } else {
+      if (splitedInput.length === 2) {
+        const number = splitedInput[0];
+        const isValidNumberReg = /^\d*\.?\d*\/?\d*\.?\d*$/;
         console.log(number);
-        if(!isValidNumberReg.test(number)) {
-          return 'invalid number'
+        if (!isValidNumberReg.test(number)) {
+          return "invalid number";
         } else {
           //console.log(eval(number).toFixed(5))
-          return eval(number).toFixed(5)
+          return eval(number).toFixed(5);
         }
       }
-      
-      
     }
-    
   };
 
   this.getUnit = function(input) {
-
-    if(units.includes(input)) {
-      //console.log(input)
-      return input
-    } else{
-      
+    const splitedInput = input.match(splitReg);
+    console.log(splitedInput)
+    if (splitedInput.length === 1) {
+      if (units.includes(input)) {
+        //console.log(input)
+        return input;
+      } else {
+        console.log("invalid unit");
+        return "invalid unit";
+      }
+    } else {
+      const unit = splitedInput[1];
+      if (units.includes(unit)) {
+        console.log(unit);
+        return unit;
+      } else {
+        console.log("invaid unit");
+        return "invalid unit";
+      }
     }
-  
-
   };
 
   this.getReturnUnit = function(initUnit) {
